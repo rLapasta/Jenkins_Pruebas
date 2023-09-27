e7bb6598f92a4d50456626565638cd9e275e0a04
Rocio Lapasta <lapastarocio@gmail.com>
2023-09-27 20:14:54 -0300

    comiteando el jenakins01

---
+++ Jenkins_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jenkins_01;
+
+/**
+ *
+ * @author User
+ */
+public class Jenkins_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Por favor que haga algo");
+    }
+}
